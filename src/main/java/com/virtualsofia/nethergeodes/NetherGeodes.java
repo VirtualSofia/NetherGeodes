3e0d76b38b868d943bbe4b357b00b20f42f2b0e1
VirtualSofia <evasnowcraft@gmail.com>
2025-11-08 01:55:38 -0800

    mod id fix 2

--- src/main/java/com/virtualsofia/nethergeodes/NetherGeodes.java
+++ src/main/java/com/virtualsofia/nethergeodes/NetherGeodes.java
@@ -34,7 +34,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 @Mod(NetherGeodes.MODID)
 public class NetherGeodes {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "nether_geode";
+    public static final String MODID = "nethergeodes";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "nether_geode" namespace
